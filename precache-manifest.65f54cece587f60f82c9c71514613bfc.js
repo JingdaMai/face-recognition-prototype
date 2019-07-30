fcaca5ca28d213933e590404eae83461c350e5ea
Jingda Mai <jingdamai@gmail.com>
2019-07-30 18:28:55 +0900

    Updates

---
+++ precache-manifest.65f54cece587f60f82c9c71514613bfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b607dca339e62d9f32e8d544c182a795",
+    "url": "/face-recognition-prototype/index.html"
+  },
+  {
+    "revision": "75e9f14cd9a2621b56c6",
+    "url": "/face-recognition-prototype/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "96971eabf60c0a99f5aa",
+    "url": "/face-recognition-prototype/static/css/main.36f00c76.chunk.css"
+  },
+  {
+    "revision": "75e9f14cd9a2621b56c6",
+    "url": "/face-recognition-prototype/static/js/2.8b6b0b2d.chunk.js"
+  },
+  {
+    "revision": "96971eabf60c0a99f5aa",
+    "url": "/face-recognition-prototype/static/js/main.d7982702.chunk.js"
+  },
+  {
+    "revision": "dbb85842ae9b57b3c79b",
+    "url": "/face-recognition-prototype/static/js/runtime~main.06445603.js"
+  }
+]);
